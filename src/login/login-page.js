b594d30ebc0ade5c5b2f06896c0114b6bb7dbd6f
Maxime medevedeff <Maxime medevedeff>
2017-02-08 18:30:35 +0100

    add webpack and gitignore + react logic

--- src/login/login-page.js
+++ src/login/login-page.js
@@ -1,3 +1,19 @@
-/**
- * Created by Klein on 02/02/2017.
- */
+import React from 'react';
+
+class LoginPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Sign in</h1>
+        <label>Username</label>
+        <input />
+        <br/>
+        <label>Password</label>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
