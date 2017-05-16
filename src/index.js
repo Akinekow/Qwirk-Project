0d8c443ba78ad7ff4486182a479638f62aaa8bf5
Aki <akinekow@gmail.com>
2017-05-16 18:12:30 +0200

    Base project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import LoginPage from './login/login-page.js'
-
-ReactDOM.render(
-  <LoginPage />,
-  document.getElementById('app')
-);
